d4cbea12af881fed1fef048e3bae57a0f4813d3d
paolodeltorto <paolo.deltorto@gmail.com>
2023-05-12 11:06:28 +0200

    creazione, registrazione e inizializzazzione progetto firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,16 @@
 import 'package:fimber/fimber.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:telegram_app/app.dart';
+import 'package:telegram_app/firebase_options.dart';
 
-void main() {
+Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
   Fimber.plantTree(DebugTree());
   runApp(const App());
 }
