ed5eebc7906f6f30453e5505ede1894bb5e1ace4
paolodeltorto <paolo.deltorto@gmail.com>
2023-05-15 17:42:25 +0200

    Costruzione dello slider con PageView

--- lib/pages/welcom_page.dart
+++ lib/pages/welcom_page.dart
@@ -31,41 +31,43 @@ class WelcomePage extends StatelessWidget {
         ),
       );
 
-  Widget _slides(BuildContext context) => Container(
-        height: 300,
-        padding: const EdgeInsets.symmetric(horizontal: 18),
-        child: Column(
-          children: [
-            const Flexible(
-                flex: 1,
-                fit: FlexFit.tight,
-                child: FaIcon(
-                  FontAwesomeIcons.telegram,
-                  size: 128,
-                  color: Colors.cyan,
-                )),
-            Flexible(
-                flex: 1,
-                fit: FlexFit.tight,
-                child: Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 30),
-                  child: Column(
-                    children: [
-                      Text(
-                        'Telegram',
-                        style: Theme.of(context).textTheme.headlineLarge,
-                      ),
-                      const Padding(
-                        padding: EdgeInsets.all(8.0),
-                        child: Text(
-                            'L\'app di messagistica più veloce al mondo. E\' gratuita e sicura'),
-                      ),
-                    ],
-                  ),
-                ))
-          ],
-        ),
-      );
+  Widget _slides(BuildContext context) {
+    final widgets = _items(context)
+        .map((item) => Container(
+              padding: const EdgeInsets.symmetric(horizontal: 18),
+              child: Column(
+                children: [
+                  Flexible(flex: 1, fit: FlexFit.tight, child: item['image']),
+                  Flexible(
+                      flex: 1,
+                      fit: FlexFit.tight,
+                      child: Padding(
+                        padding: const EdgeInsets.symmetric(horizontal: 30),
+                        child: Column(
+                          children: [
+                            Text(
+                              item['header'],
+                              style: Theme.of(context).textTheme.headlineLarge,
+                            ),
+                            Padding(
+                              padding: const EdgeInsets.all(8.0),
+                              child: Text(item['description']),
+                            ),
+                          ],
+                        ),
+                      ))
+                ],
+              ),
+            ))
+        .toList(growable: false);
+    return Container(
+      height: 300,
+      child: PageView.builder(
+        itemBuilder: (context, index) => widgets[index],
+        itemCount: widgets.length,
+      ),
+    );
+  }
 
   Widget _indicator() => Container();
 
@@ -73,4 +75,61 @@ class WelcomePage extends StatelessWidget {
         onPressed: () {},
         child: Text(AppLocalizations.of(context)?.action_start_chatting ?? ''),
       );
+
+  List _items(BuildContext context) => [
+        {
+          'image': const FaIcon(
+            FontAwesomeIcons.telegram,
+            size: 128,
+            color: Colors.cyan,
+          ),
+          'header': AppLocalizations.of(context)?.welcome_header_1,
+          'description': AppLocalizations.of(context)?.welcome_description_1,
+        },
+        {
+          'image': const FaIcon(
+            FontAwesomeIcons.rocketchat,
+            size: 128,
+            color: Colors.red,
+          ),
+          'header': AppLocalizations.of(context)?.welcome_header_2,
+          'description': AppLocalizations.of(context)?.welcome_description_2,
+        },
+        {
+          'image': const FaIcon(
+            FontAwesomeIcons.commentDollar,
+            size: 128,
+            color: Colors.green,
+          ),
+          'header': AppLocalizations.of(context)?.welcome_header_3,
+          'description': AppLocalizations.of(context)?.welcome_description_3,
+        },
+        {
+          'image': const FaIcon(
+            FontAwesomeIcons.tachographDigital,
+            size: 128,
+            color: Colors.red,
+          ),
+          'header': AppLocalizations.of(context)?.welcome_header_4,
+          'description': AppLocalizations.of(context)?.welcome_description_4,
+        },
+        {
+          'image': const FaIcon(
+            FontAwesomeIcons.lock,
+            size: 128,
+            color: Colors.orange,
+          ),
+          'header': AppLocalizations.of(context)?.welcome_header_5,
+          'description': AppLocalizations.of(context)?.welcome_description_5,
+        },
+        {
+          'image': const FaIcon(
+            FontAwesomeIcons.cloud,
+            size: 128,
+            color: Colors.grey,
+          ),
+          'header': AppLocalizations.of(context)?.welcome_header_6,
+          'description': AppLocalizations.of(context)?.welcome_description_6,
+        }
+      ];
 }
